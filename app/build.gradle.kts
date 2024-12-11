3b6df1e2320a2aea3c3e82c45ea4cdca72aa6e2b
Duy Tho <duythopm123@gmail.com>
2024-12-11 20:07:55 +0700

    Login, Settings, Create account & Add employee

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
 dependencies {
     val room_version = "2.6.1"
     val lifecycle_version = "2.8.5"
-
+    implementation ("com.google.android.material:material:1.12.0")
     // ViewModel
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
     // LiveData
