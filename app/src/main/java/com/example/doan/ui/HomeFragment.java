e83a82046f95966fa6c8a7590dce8511c72f665e
Yoirk <118036935+Yoirk@users.noreply.github.com>
2025-01-02 18:34:33 +0700

    Thêm xml reports

--- app/src/main/java/com/example/doan/ui/HomeFragment.java
+++ app/src/main/java/com/example/doan/ui/HomeFragment.java
@@ -14,17 +14,28 @@ import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.doan.MainActivity;
 import com.example.doan.MainActivityForSaleEmployee;
 import com.example.doan.R;
 import com.example.doan.customers.AddNewCustomerActivity;
+import com.example.doan.reports.EmployeeReportAdapter;
+import com.example.doan.reports.Reports;
+import com.example.doan.reports.ReportsViewModel;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private TextView textViewName;
+    private ReportsViewModel reportsViewModel;
+    private RecyclerView recyclerView;
+    private EmployeeReportAdapter reportsAdapter; // Adapter cho RecyclerView
 
     @Nullable
     @Override
@@ -32,6 +43,42 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.home_fragment, container, false);
 
         textViewName = view.findViewById(R.id.tvTenNV);
+        recyclerView = view.findViewById(R.id.recycler_view_notifications);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        reportsAdapter = new EmployeeReportAdapter(); // Khởi tạo adapter
+        recyclerView.setAdapter(reportsAdapter);
+
+        // Khởi tạo ViewModel
+        reportsViewModel = new ViewModelProvider(this).get(ReportsViewModel.class);
+
+        // Quan sát LiveData từ ViewModel
+        reportsViewModel.getAllreports().observe(getViewLifecycleOwner(), new Observer<List<Reports>>() {
+            @Override
+            public void onChanged(List<Reports> reports) {
+                // Cập nhật dữ liệu cho Adapter khi có thay đổi
+                reportsAdapter.submitList(reports);
+            }
+        });
+
+        // Thiết lập sự kiện click cho item
+        reportsAdapter.setOnItemClickListener(report -> {
+            ReportDetailEmployeeFragment fragment = new ReportDetailEmployeeFragment();
+
+            Bundle bundle = new Bundle();
+            bundle.putString("id", report.getReportID());
+            bundle.putString("title", report.getReportTitle());
+            bundle.putString("startDate", report.getStartDate());
+            bundle.putString("endDate", report.getEndDate());
+            bundle.putString("description", report.getReportContent());
+            fragment.setArguments(bundle);
+
+            getParentFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frameLayout4, fragment)
+                    .addToBackStack(null)
+                    .commit();
+        });
+
         if (getArguments() != null) {
             String userName = getArguments().getString("USER_NAME");
             if (userName != null) {
